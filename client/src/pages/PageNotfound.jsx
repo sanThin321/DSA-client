0038a4144842fcd584249c85f6e5f675ea172867
Purna Bdr <purnabdrrana53@gmail.com>
2024-11-18 01:25:51 +0600

    Completed

--- client/src/pages/PageNotfound.jsx
+++ client/src/pages/PageNotfound.jsx
@@ -3,7 +3,7 @@ const PageNotFound = () => {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404</h1>
       <p>Page not found</p>
-      <a href="/">Go back to the homepage</a>
+      <a href="/dashboard">Go back to the homepage</a>
     </div>
   );
 };
